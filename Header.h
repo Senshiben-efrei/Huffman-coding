61c6917cc3d3e17e07e3522469e9355ef5214281
Ayman <ayman.ben.hajjaj@efrei.net>
2020-11-17 18:51:33 +0100

    Update Header.h

---
+++ Header.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+/*
+Écrire une fonction qui lit un texte dans un fichier, et qui le traduit en son équivalent 0 et 1 dans un autre fichier
+prend text.txt 
+rend binary.txt
+
+*/
+void text_to_binary(text.txt ; binary.txt )
+{
+
+}
