3917474971b7d13711ac16c194336e376876e516
benjaminlsk <73712469+benjaminlsk@users.noreply.github.com>
2020-11-22 15:38:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main()
+
+
+typedef struct Element{
+    int data; //Noeud
+    struct Element *next; //Pointeur vers l'élément suivant
+} Element;
+
+typedef Element* List;
+
+int list_occurences(Element* l, int n){
+    if (l == NULL) return 0;
+    if (l -> data == n) return 1 + list_occurences(l -> next, n);
+    return 0 + list_occurences(l -> next, n);
+}
+
+
+int main (void)
 {
-	/* code */
-	printf("J'adore le génie logiciel <3");
-	return 0;
+
+    list_occurences(struct Element, 5);
+
+    return 0;
 }
